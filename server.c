e23e36103a60388111ea4ed4bac8fbf439e2da30
Lucas Pimentel <luk.2001@hotmail.com>
2023-06-24 12:34:07 -0300

    Adiciona print de nome e função de criar sala

--- server.c
+++ server.c
@@ -8,6 +8,27 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #define STDIN 0
+#define MAX 100
+#define MAX_SIZE 80
+
+typedef struct
+{
+    int cliente_sd;
+    char nome[256];
+    int ativo;
+} cliente;
+
+typedef struct
+{
+    fd_set sala_fd;
+    int limite;
+    int quantidade;
+    char nome[MAX_SIZE];
+    int ativo;
+    cliente *clientes;
+} sala;
+
+sala salas[MAX];
 
 fd_set master, read_fds, write_fds;
 struct sockaddr_in myaddr, remoteaddr;
@@ -30,6 +51,36 @@ void envia_msg()
     }
 }
 
+void abre_salas()
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        FD_ZERO(&salas[i].sala_fd);
+        salas[i].ativo = 0;
+        salas[i].limite = 0;
+        salas[i].quantidade = 0;
+    }
+}
+
+int cria_sala(int limite, char nome_sala[])
+{
+    int sala_id;
+    for (sala_id = 0; sala_id < MAX; sala_id++)
+        if (salas[sala_id].ativo == 0)
+            break;
+
+    salas[sala_id].ativo = 1;
+    salas[sala_id].limite = limite;
+    strcpy(salas[sala_id].nome, nome_sala);
+    salas[sala_id].clientes = malloc(limite * sizeof(cliente));
+
+    for (int i = 0; i < limite; i++)
+        salas[sala_id].clientes[i].ativo = 0;
+
+    printf("Nova sala %d criada\n", sala_id);
+    return sala_id;
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -41,6 +92,8 @@ int main(int argc, char *argv[])
 
     FD_ZERO(&master);
     FD_ZERO(&read_fds);
+
+    abre_salas();
     sd = socket(AF_INET, SOCK_STREAM, 0);
     setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
@@ -69,6 +122,30 @@ int main(int argc, char *argv[])
                 {
                     newfd = accept(sd, (struct sockaddr *)&remoteaddr, &addrlen);
                     FD_SET(newfd, &master);
+
+                    char menu_nome[MAX_SIZE] = "Digite seu nome:\n";
+                    send(newfd, &menu_nome, strlen(menu_nome), 0);
+                    char nome_usuario[256], nome_sala[256];
+                    int size_nome_usuario = recv(newfd, nome_usuario, 256, 0);
+
+                    char menu_ops[MAX_SIZE] = "SELECIONE A OPCAO DESEJADA: \n 1 - Criar Sala \n 2 - Entrar em uma Sala\n";
+                    send(newfd, &menu_ops, strlen(menu_ops), 0);
+                    recv(newfd, buf, 256, 0);
+                    int opcao_menu = atoi(buf);
+
+                    char menu_sala_nome[MAX_SIZE] = "Digite o nome da Sala\n";
+                    send(newfd, &menu_sala_nome, strlen(menu_sala_nome), 0);
+                    int size_nome_sala = recv(newfd, menu_sala_nome, 256, 0);
+
+                    if (opcao_menu == 1)
+                    {
+                        char menu_sala_limite[MAX_SIZE] = "Digite o limite de participantes da Sala\n";
+                        send(newfd, &menu_sala_limite, strlen(menu_sala_limite), 0);
+                        recv(newfd, buf, 256, 0);
+                        int limite = atoi(buf);
+                        int sala_id = cria_sala(limite, nome_sala);
+                    }
+
                     if (newfd > fdmax)
                         fdmax = newfd;
                 }
